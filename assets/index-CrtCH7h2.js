6955028120c8c8a1e2c1f7ba09886521f8991534
heronmolina <heron.molina@gmail.com>
2026-07-23 14:01:05 -0600

    Catálogo ágil: chips de área + búsqueda sin acentos + etiquetas

--- assets/index-CrtCH7h2.js
+++ assets/index-CrtCH7h2.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-BxvGGuMG.js";/*! *****************************************************************************
+import{g as Xu}from"./index-tXB5TuN4.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
