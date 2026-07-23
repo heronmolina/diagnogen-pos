863f4c16891120d989cf20c98f9a072d43ca734e
heronmolina <heron.molina@gmail.com>
2026-07-23 12:45:27 -0600

    Precio subsecuente (switch en caja + campo en catálogo)

--- assets/index-BwBJB-e0.js
+++ assets/index-BwBJB-e0.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DIZNbgX7.js";/*! *****************************************************************************
+import{g as Xu}from"./index-BxvGGuMG.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
