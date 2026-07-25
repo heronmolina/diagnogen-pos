ea55a2582d636f46013e211035bc977e5c8f1542
heronmolina <heron.molina@gmail.com>
2026-07-25 14:08:10 -0600

    Receta de insumos + Candado 3 anomalías (209f5a2)

--- assets/index-DF5ZP8Rm.js
+++ assets/index-DF5ZP8Rm.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-CQsLQ3iE.js";/*! *****************************************************************************
+import{g as Xu}from"./index-Bg3meJ6c.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
