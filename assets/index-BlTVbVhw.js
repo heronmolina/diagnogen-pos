24439358346f61534141fbbd15675483411a229f
heronmolina <heron.molina@gmail.com>
2026-07-29 10:48:51 -0600

    Catálogo admin con buscador y filtros de área (32c32d3)

--- assets/index-BlTVbVhw.js
+++ assets/index-BlTVbVhw.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-B2LkXvEO.js";/*! *****************************************************************************
+import{g as Xu}from"./index-CCkPES45.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
