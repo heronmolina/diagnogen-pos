77f4ddbf1c205a84502ecd5bf893d920b2c4201e
heronmolina <heron.molina@gmail.com>
2026-07-29 12:21:48 -0600

    Deploy comanda automática por Cloud API (1c03f42)

--- assets/index-VidorIua.js
+++ assets/index-VidorIua.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-D-AeYj7N.js";/*! *****************************************************************************
+import{g as Xu}from"./index-DN-L-uqS.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
