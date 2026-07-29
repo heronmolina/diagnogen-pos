ac4f61412234ec2ea2561089ba6020559d476691
heronmolina <heron.molina@gmail.com>
2026-07-28 20:41:36 -0600

    Deploy lote J: tablero fluido, ventas del día, ticket premium, reportes unificados (50e536c)

--- assets/index-BHuc6EB9.js
+++ assets/index-BHuc6EB9.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DJfwJItt.js";/*! *****************************************************************************
+import{g as Xu}from"./index-qMpmcnBP.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
