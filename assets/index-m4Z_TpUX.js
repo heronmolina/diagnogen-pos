c6fa94088a5c20fc5e1a8b28b3c1f7d1e364ab72
heronmolina <heron.molina@gmail.com>
2026-07-29 13:21:35 -0600

    Deploy PR-L: Listo exige estudio subido + recibo pulido (8306673)

--- assets/index-m4Z_TpUX.js
+++ assets/index-m4Z_TpUX.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DN-L-uqS.js";/*! *****************************************************************************
+import{g as Xu}from"./index-DGgEvquz.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
