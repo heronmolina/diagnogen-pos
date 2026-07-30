1784f00cc1d0b027a14b32fa7fdb3ef30ad6cc9b
heronmolina <heron.molina@gmail.com>
2026-07-29 19:16:25 -0600

    Deploy lote M: cotizaciones, modal centrado, brillo, archivos/enlaces en resultados (441113f)

--- assets/index-DbT08RLk.js
+++ assets/index-DbT08RLk.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DGgEvquz.js";/*! *****************************************************************************
+import{g as Xu}from"./index-DQlWpfIK.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
